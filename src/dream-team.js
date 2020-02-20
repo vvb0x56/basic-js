89aa0b4327affe72c3060170857cf5e582fc36c8
Vladimir Burlakov <vvb0x56@gmail.com>
2020-02-20 10:41:36 +0300

    feat: add solution for dream-team task.

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,10 @@
 module.exports = function createDreamTeam(members) {
-  if (!Array.isArray(members))  return false;
+  if (!Array.isArray(members))        return false;
 
-  return members.map(function() {}).sort();
+  return members.map(function(element) {
+    if (typeof(element) !== 'string') return;
+    
+    let c = element.trimLeft().charAt(0).toUpperCase();
+    if (c >= 'A' && c <= 'Z')         return c;
+  }).sort().join("");
 };
